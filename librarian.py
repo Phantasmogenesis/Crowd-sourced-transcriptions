c5f1d077f3f03445d076db07cf04a29b508ba475
ProfessorAD <65755354+ProfessorAD@users.noreply.github.com>
2021-11-06 15:04:49 -0400

    Complete

---
+++ librarian.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, render_template, request, redirect
+from flask.helpers import url_for
+import librarian_functions
+import flask_login
+
+
+librarian = Blueprint("librarian", __name__)
+
+@librarian.route("/librarian", methods=['POST', 'GET'])
+@flask_login.login_required
+def librarian_page():
+    if request.method == "POST":
+        if request.form.get("Yes") == "Yes":
+            librarian_functions.approve_transcription(librarian_functions.txt_file_id, librarian_functions.jpg_file_id)
+            return redirect(url_for("home.home_page"))
+        elif request.form.get("No") == "No":
+            librarian_functions.disapprove_transcription(librarian_functions.txt_file_id, librarian_functions.jpg_file_id)
+            return redirect(url_for("home.home_page"))
+
+    
+    librarian_functions.get_transcription()
+
+    return render_template("librarian.html", jpg_link=librarian_functions.jpg_view_link, txt_content=librarian_functions.txt_file_content)
--- review.py
+++ review.py
@@ -1,9 +1,6 @@
 from flask import Blueprint, render_template, request, redirect
 from flask.helpers import url_for
 import review_functions
-# from transc.py import get_random_jpg_id, download_jpg
-# import display_img as display_img
-# import drive_functions as df
 
 review = Blueprint("review", __name__)
 
@@ -11,7 +8,6 @@ review = Blueprint("review", __name__)
 
 def review_page():
     if request.method == "POST":
-        # print("t")
         if request.form.get("Yes") == "Yes":
             review_functions.increment_review_counter(review_functions.txt_file_id, review_functions.jpg_file_id)
             return redirect(url_for("home.home_page"))
@@ -21,12 +17,5 @@ def review_page():
 
     
     review_functions.get_transcription()
-
-    # review_functions.submit_review("a")
-
-    # review_functions.get_transcription_txt()
-    # review_functions.read_txt_file(review_functions.txt_file_id)
-    # review_functions.increment_review_counter(review_functions.txt_file_id, review_functions.jpg_file_id)
-
 
     return render_template("review.html", jpg_link=review_functions.jpg_view_link, txt_content=review_functions.txt_file_content)
