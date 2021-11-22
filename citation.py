f71080f7654050cd6cea160c027b91b344a50b0e
Alex Dombroski <ProfessorARD@gmail.com>
2021-11-22 09:00:39 -0500

    Fix

--- citation.py
+++ citation.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request, redirect
 
 citation = Blueprint("citation", __name__)
 
-@citation.route("/citations", methods=['POST', 'GET'])
+@citation.route("/citation", methods=['POST', 'GET'])
 
 def citation_page():
     if request.method == "GET":
